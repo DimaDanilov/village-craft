8f45ec45acac2dd85213f6326dc9d689fb7a375c
Dima Danilov <nervogryz@gmail.com>
2025-05-09 02:11:23 +0300

    add x icon pointer cursor

--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -20,7 +20,7 @@ export const Modal = ({ title, isOpen, onClose, error, children }: ModalProps) =
       <div className="bg-white pt-3 pb-8 px-6 rounded-lg shadow-lg w-full max-w-lg" onClick={onStopPropagationClick}>
         <div className="flex justify-between">
           <h2 className="text-2xl">{title}</h2>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+          <button onClick={onClose} className="text-gray-500 hover:text-gray-700 cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
