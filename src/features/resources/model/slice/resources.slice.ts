20e3892549e5e2b82a93071c9b29376b6bfd06c3
Dima Danilov <nervogryz@gmail.com>
2025-05-08 22:18:52 +0300

    add check to sell only existing resources

--- src/features/resources/model/slice/resources.slice.ts
+++ src/features/resources/model/slice/resources.slice.ts
@@ -1,23 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { CoinsStorage, StonePile, WoodPile } from '../types';
-import { selectCoinsCount, selectStoneCount, selectWoodCount } from '../selectors';
+import { selectCoinsCount, selectResourcesError, selectStoneCount, selectWoodCount } from '../selectors';
 
 export interface ResourcesState {
-  wood: WoodPile;
-  stone: StonePile;
-  coins: CoinsStorage;
+  resources: {
+    wood: WoodPile;
+    stone: StonePile;
+    coins: CoinsStorage;
+  };
+  error?: string;
 }
 
 const initialState: ResourcesState = {
-  wood: {
-    count: 0,
+  resources: {
+    wood: {
+      count: 0,
+    },
+    stone: {
+      count: 0,
+    },
+    coins: {
+      count: 3,
+    },
   },
-  stone: {
-    count: 0,
-  },
-  coins: {
-    count: 3,
-  },
+  error: undefined,
 };
 
 export const resourcesSlice = createSlice({
@@ -27,16 +33,41 @@ export const resourcesSlice = createSlice({
     selectWoodCount,
     selectStoneCount,
     selectCoinsCount,
+    selectResourcesError,
   },
   reducers: {
     chopWood: (state) => {
-      state.wood.count += 1;
+      state.resources.wood.count += 1;
     },
     mineStone: (state) => {
-      state.stone.count += 1;
+      state.resources.stone.count += 1;
     },
-    sellItems: (state) => {
-      state.coins.count += 1;
+    sellWood: (state, action: PayloadAction<{ woodCount: number }>) => {
+      const { woodCount } = action.payload;
+
+      if (state.resources.wood.count < woodCount) {
+        state.error = 'Not enough wood to sell';
+        return;
+      }
+
+      state.resources.wood.count -= woodCount;
+      state.resources.coins.count += woodCount;
+      state.error = undefined;
+    },
+    sellStone: (state, action: PayloadAction<{ stoneCount: number }>) => {
+      const { stoneCount } = action.payload;
+
+      if (state.resources.stone.count < stoneCount) {
+        state.error = 'Not enough stone to sell';
+        return;
+      }
+
+      state.resources.stone.count -= stoneCount;
+      state.resources.coins.count += stoneCount;
+      state.error = undefined;
+    },
+    clearError: (state) => {
+      state.error = undefined;
     },
   },
 });
