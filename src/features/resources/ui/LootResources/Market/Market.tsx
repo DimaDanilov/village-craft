6fea02e2021da29e198dfd2952d594c4251f2eca
Dima Danilov <nervogryz@gmail.com>
2025-05-08 23:20:50 +0300

    update structure

--- src/features/resources/ui/LootResources/Market/Market.tsx
+++ src/features/resources/ui/LootResources/Market/Market.tsx
@@ -1,44 +1,22 @@
 import { resourcesSlice } from '@features/resources/model';
 import { Button } from '@shared/Button/Button';
-import { Modal } from '@shared/Modal/Modal';
-import { useAppDispatch, useAppSelector } from '@store';
 import { useCallback, useState } from 'react';
+import { MarketModal } from './MarketModal';
+import { useAppDispatch } from '@store';
 
 export const Market = () => {
   const dispatch = useAppDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const error = useAppSelector(resourcesSlice.selectors.selectResourcesError);
 
   const openMarket = useCallback(() => {
     dispatch(resourcesSlice.actions.clearError());
     setIsModalOpen(true);
   }, [dispatch]);
 
-  const closeMarket = useCallback(() => {
-    dispatch(resourcesSlice.actions.clearError());
-    setIsModalOpen(false);
-  }, [dispatch]);
-
-  const sellWood = useCallback(
-    () => dispatch(resourcesSlice.actions.sellWood({ woodCount: 1 })),
-    [dispatch, resourcesSlice],
-  );
-
-  const sellStone = useCallback(
-    () => dispatch(resourcesSlice.actions.sellStone({ stoneCount: 1 })),
-    [dispatch, resourcesSlice],
-  );
-
   return (
     <>
       <Button onClick={openMarket}>Market</Button>
-      <Modal title="Market" isOpen={isModalOpen} onClose={closeMarket}>
-        {error && <div className="mb-4 p-2 bg-red-100 text-red-700 rounded">{error}</div>}
-        <div className="flex flex-col gap-2">
-          <Button onClick={sellWood}>Sell Wood</Button>
-          <Button onClick={sellStone}>Sell Stone</Button>
-        </div>
-      </Modal>
+      <MarketModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </>
   );
 };
--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -15,9 +15,9 @@ export const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
       className="fixed inset-0 flex items-center justify-center z-50 bg-black/50 backdrop-blur-sm"
       // onClick={onClose}
     >
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+      <div className="bg-white pt-3 pb-3 px-6 rounded-lg shadow-lg w-full max-w-md">
         <div className="flex justify-between">
-          <h2>{title}</h2>
+          <h2 className="text-2xl">{title}</h2>
           <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ export const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
             </svg>
           </button>
         </div>
-        <div className="mt-4">{children}</div>
+        <div className="mt-14">{children}</div>
       </div>
     </div>
   );
