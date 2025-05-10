57644d743545a8a06081d4f5cb754d9b00c4e7aa
Dima Danilov <nervogryz@gmail.com>
2025-05-10 22:59:43 +0300

    update color palette

--- src/shared/DeckCard/constants/DeckCard.constants.ts
+++ src/shared/DeckCard/constants/DeckCard.constants.ts
@@ -1,9 +1,9 @@
 import type { DeckCardPalette } from '../types';
 
 export const DECK_CARD_RESOURCE_PALETTE: DeckCardPalette = {
-  bgColorClassName: 'bg-orange-300',
-  textColorClassName: 'text-amber-700',
-  borderColorClassName: 'border-amber-400',
+  bgColorClassName: 'bg-orange-200',
+  textColorClassName: 'text-amber-600',
+  borderColorClassName: 'border-amber-300',
 };
 export const DECK_CARD_SERVICE_PALETTE: DeckCardPalette = {
   bgColorClassName: 'bg-sky-200',
