5dbc263fb456c69a74a0ef44f006d8c99ab11241
Dima Danilov <nervogryz@gmail.com>
2025-05-27 15:07:38 +0300

    add table to building

--- src/features/buildings/ui/AddService/ServiceBuilderModal/ServiceBuilderModal.tsx
+++ src/features/buildings/ui/AddService/ServiceBuilderModal/ServiceBuilderModal.tsx
@@ -2,6 +2,7 @@ import { buildingsSlice } from '@features/buildings/model';
 import { Button } from '@shared/Button/Button';
 import { Modal } from '@shared/Modal/Modal';
 import { useAppDispatch, useAppSelector } from '@store';
+import { DECK_CARD_INFOS } from '@widgets';
 import { useCallback } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 
@@ -35,14 +36,48 @@ export const ServiceBuilderModal = ({ isModalOpen, setIsModalOpen }: AddServiceM
 
   return (
     <Modal title="Service Builder" isOpen={isModalOpen} onClose={onCloseServiceBuilder}>
-      <div className="flex flex-col gap-2">
-        <Button onClick={onBuildForge} disabled={isBuildForgeDisabled}>
-          Build Forge
-        </Button>
-        <Button onClick={onBuildMarket} disabled={isBuildMarketDisabled}>
-          Build Market
-        </Button>
-      </div>
+      <table className="text-center border-separate border-spacing-x-8 border-spacing-y-8">
+        <thead>
+          <tr>
+            <th className="w-1/5 px-6">Service Building</th>
+            <th className="w-1/5 px-6">Description</th>
+            <th className="w-2/5 px-6">Cost</th>
+            <th className="w-1/5 px-6"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className="w-60 flex flex-col gap-1 justify-center items-center">
+                <img width={300} src={DECK_CARD_INFOS.forge.imageSrc} alt="Forge Image" />
+                <span className="text-lg">{DECK_CARD_INFOS.forge.title}</span>
+              </div>
+            </td>
+            <td>{DECK_CARD_INFOS.forge.description}</td>
+            <td></td>
+            <td>
+              <Button onClick={onBuildForge} disabled={isBuildForgeDisabled}>
+                Build {DECK_CARD_INFOS.forge.title}
+              </Button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="w-60 flex flex-col gap-1 justify-center items-center">
+                <img width={300} src={DECK_CARD_INFOS.market.imageSrc} alt="Market Image" />
+                <span className="text-lg">{DECK_CARD_INFOS.market.title}</span>
+              </div>
+            </td>
+            <td>{DECK_CARD_INFOS.market.description}</td>
+            <td></td>
+            <td>
+              <Button onClick={onBuildMarket} disabled={isBuildMarketDisabled}>
+                Build {DECK_CARD_INFOS.market.title}
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </Modal>
   );
 };
