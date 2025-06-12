8811ec0e960ce1c7e5461c70ab9c0ec430ddb696
dima danilov <dima.danilov.2012@mail.ru>
2025-06-13 02:47:15 +0300

    update cards layout

--- src/features/buildings/ui/DeckCard/DeckCardHeader/DeckCardHeader.tsx
+++ src/features/buildings/ui/DeckCard/DeckCardHeader/DeckCardHeader.tsx
@@ -8,9 +8,9 @@ export const DeckCardHeader = ({ imageSrc, title, description }: DeckCardHeaderP
   return (
     <div className={`flex flex-col`}>
       <img className="rounded-t-xl" src={imageSrc} alt={title} />
-      <div className="flex flex-col gap-4 mx-3 mt-5 h-24">
-        <h2 className="text-2xl font-semibold text-center">{title}</h2>
-        <p className="text-justify">{description}</p>
+      <div className="flex flex-col gap-4 mx-3 mt-2 h-24">
+        <h2 className="text-xl font-semibold text-center">{title}</h2>
+        <p className="text-justify text-sm">{description}</p>
       </div>
     </div>
   );
--- src/features/buildings/ui/DeckCard/DeckResourceCard.tsx
+++ src/features/buildings/ui/DeckCard/DeckResourceCard.tsx
@@ -21,12 +21,12 @@ export const DeckResourceCard = ({
     <DeckCardDefault buildingInfo={buildingInfo} deckCardPalette={DECK_CARD_RESOURCE_PALETTE} onClick={onClick}>
       <div className="flex flex-row justify-end items-center mx-3 mt-5 gap-1">
         <ResourceIconComponent
-          width={30}
-          height={30}
+          width={25}
+          height={25}
           className={`fill-transparent ${iconStrokeColorClassName}`}
           strokeWidth="60px"
         />
-        <span>{resourceCount}</span>
+        <span className='text-sm'>{resourceCount}</span>
       </div>
     </DeckCardDefault>
   );
